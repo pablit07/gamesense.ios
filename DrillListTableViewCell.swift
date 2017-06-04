10d3155cd77abbba4b90afbf2f49b9e4a13c06e6
Paul Kohlhoff <paul.kohlhoff@gmail.com>
2017-06-04 07:41:45 -0600

    download button and progress bar; add full videos to download

--- DrillListTableViewCell.swift
+++ DrillListTableViewCell.swift
@@ -10,14 +10,18 @@ import UIKit
 
 class DrillListTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var progressView: UIProgressView!
     @IBOutlet weak var startDownload: UIButton!
     @IBAction func startDownloadButtonTap(_ sender: UIButton) {
+        self.startDownload.isHidden = true
         if self.drillId != nil {
-            getDrillListQuestions(self.getDrillListQuestions())
+            self.getDrillListQuestions()
         }
     }
     
     var drillId : Int? = nil
+    var numberToDownload = 0
+    var completedDownloads = 0
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -34,6 +38,8 @@ class DrillListTableViewCell: UITableViewCell {
     private func getDrillListQuestions() {
         var drillId = self.drillId!
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        self.progressView.isHidden = false
+        self.clearNumberForDownload()
         SharedNetworkConnection.apiGetDrillQuestions(apiToken: appDelegate.apiToken, drillID: drillId, completionHandler: { data, response, error in
             guard let data = data, error == nil else {                                                 // check for fundamental networking error
                 print("error=\(error)")
@@ -42,12 +48,16 @@ class DrillListTableViewCell: UITableViewCell {
             
             var drillQuestionsParser = DrillQuestionParser(jsonString: String(data: data, encoding: .utf8)!)
             var drillQuestionsArray = (drillQuestionsParser?.getDrillQuestionArray())!
+            self.updateNumberForDownload(numberForDownload: (drillQuestionsArray.count * 2))
+            var filenameArray = [String]()
             for drillQuestion in drillQuestionsArray {
+                filenameArray.append(drillQuestion.occludedVideo)
+                filenameArray.append(drillQuestion.fullVideo)
+            }
+            for filename in filenameArray {
                 DispatchQueue.main.async {
                     var cacheDirectory = try! FileManager.default.url(for: .cachesDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-                    let filename = drillQuestion.occludedVideo
                     cacheDirectory.appendPathComponent(filename)
-                    print(filename)
                     SharedNetworkConnection.downloadVideo(resourceFilename: filename, completionHandler: { data, response, error in
                         guard let data = data, error == nil else {                                                 // check for fundamental networking error
                             print("error=\(error)")
@@ -61,12 +71,30 @@ class DrillListTableViewCell: UITableViewCell {
                         }
                         
                         try? data.write(to: cacheDirectory)
+                        DispatchQueue.main.async { self.updateNumberDownloaded() }
                     })
                 }
             }
             
         })
     }
-
-
+    
+    private func updateNumberForDownload(numberForDownload: Int) {
+        self.numberToDownload = numberForDownload
+    }
+    
+    private func updateNumberDownloaded() {
+        self.completedDownloads += 1
+        let progress = (Float(self.completedDownloads) / Float(self.numberToDownload))
+        self.progressView.setProgress(progress, animated: true)
+        if self.completedDownloads == self.numberToDownload {
+            self.clearNumberForDownload()
+            self.progressView.isHidden = true
+        }
+    }
+    
+    private func clearNumberForDownload() {
+        self.progressView.progress = 0
+        self.numberToDownload = 0
+    }
 }
