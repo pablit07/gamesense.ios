8dc129718be8c6348317f347a02881bf39b36f77
Paul Kohlhoff <paul.kohlhoff@gmail.com>
2017-08-20 11:08:07 -0600

    remove download cache merge

--- DrillListTableViewCell.swift
+++ DrillListTableViewCell.swift
@@ -10,12 +10,6 @@ import UIKit
 
 class DrillListTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var startDownload: UIButton!
-    @IBAction func startDownloadButtonTap(_ sender: UIButton) {
-        if self.drillId != nil {
-            getDrillListQuestions(self.getDrillListQuestions())
-        }
-    }
     
     var drillId : Int? = nil
     
@@ -29,44 +23,5 @@ class DrillListTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-    
-    
-    private func getDrillListQuestions() {
-        var drillId = self.drillId!
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        SharedNetworkConnection.apiGetDrillQuestions(apiToken: appDelegate.apiToken, drillID: drillId, completionHandler: { data, response, error in
-            guard let data = data, error == nil else {                                                 // check for fundamental networking error
-                print("error=\(error)")
-                return
-            }
-            
-            var drillQuestionsParser = DrillQuestionParser(jsonString: String(data: data, encoding: .utf8)!)
-            var drillQuestionsArray = (drillQuestionsParser?.getDrillQuestionArray())!
-            for drillQuestion in drillQuestionsArray {
-                DispatchQueue.main.async {
-                    var cacheDirectory = try! FileManager.default.url(for: .cachesDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-                    let filename = drillQuestion.occludedVideo
-                    cacheDirectory.appendPathComponent(filename)
-                    print(filename)
-                    SharedNetworkConnection.downloadVideo(resourceFilename: filename, completionHandler: { data, response, error in
-                        guard let data = data, error == nil else {                                                 // check for fundamental networking error
-                            print("error=\(error)")
-                            return
-                        }
-                        
-                        if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
-                            // 403 on no token
-                            print("statusCode should be 200, but is \(httpStatus.statusCode)")
-                            print("response = \(response)")
-                        }
-                        
-                        try? data.write(to: cacheDirectory)
-                    })
-                }
-            }
-            
-        })
-    }
-
 
 }
